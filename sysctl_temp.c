19c7744a27df073feb9aefaba6f52ced85a7ec5d
Maxim Kirenenko <part1zancheg@gmail.com>
2017-09-24 23:29:19 +0300

    added a C program to read sysctls

---
+++ sysctl_temp.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
+
+int main(int argc, char **argv) {
+	int bmp085temp;
+	int bmp085pressure;
+	int ds18b20temp;
+	size_t len;
+	len = sizeof(int);
+
+	sysctlbyname("dev.bmp085.0.temperature", &bmp085temp, &len, NULL, 0);
+	sysctlbyname("dev.bmp085.0.pressure", &bmp085pressure, &len, NULL, 0);
+	sysctlbyname("dev.ow_temp.0.temperature", &ds18b20temp, &len, NULL, 0);
+	printf("Current bmp085 temperature: %3.3f\n", (bmp085temp-2730)/10.0f);
+	printf("Current bmp085 pressure: %d\n", bmp085pressure);
+	printf("Current ds18b20 temperature: %3.3f\n", (ds18b20temp-273150)/1000.0f);
+	return 0;
+}
