e5e79d0a11c00281a2697963cd99d0aae5d81d04
Maxim V Filimonov <che@bein.link>
2017-09-25 19:04:55 +0300

    add some precision to sysctl_temp

--- sysctl_temp.c
+++ sysctl_temp.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv) {
 	sysctlbyname("dev.bmp085.0.temperature", &bmp085temp, &len, NULL, 0);
 	sysctlbyname("dev.bmp085.0.pressure", &bmp085pressure, &len, NULL, 0);
 	sysctlbyname("dev.ow_temp.0.temperature", &ds18b20temp, &len, NULL, 0);
-	printf("Current bmp085 temperature: %3.3f\n", (bmp085temp-2730)/10.0f);
+	printf("Current bmp085 temperature: %3.3f\n", (bmp085temp-2731.5f)/10.0f);
 	printf("Current bmp085 pressure: %d\n", bmp085pressure);
 	printf("Current ds18b20 temperature: %3.3f\n", (ds18b20temp-273150)/1000.0f);
 	return 0;
